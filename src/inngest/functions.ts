4aa64db82d35b499df245507354976d6dfffd8fa
SameedIlyas <silyas.bese22seecs@seecs.edu.pk>
2025-07-20 02:21:53 +0500

    feat: integrate Inngest agent kit and update invoke mutation
    - Added @inngest/agent-kit dependency to package.json.
    - Updated page.tsx to include an input field for user input and modified the button's onClick handler to use the input value.
    - Enhanced helloWorld function in functions.ts to utilize the Inngest agent for generating code based on user input.
    - Changed the input parameter in the invoke mutation from 'text' to 'value' to align with the new input structure.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer that focuses on practical and scalable approaches with best modern practices in software engineering.  You write readable, maintanable code with amazing UI. You only respond with code snippets.",
+      model: gemini({ model: "gemini-2.5-pro" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following code and do not give anything other than the required code of the following: ${event.data.value}`,
+    );
+    console.log(output);
+
+    return { output};
   },
 );
