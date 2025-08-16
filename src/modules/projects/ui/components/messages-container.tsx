d9e07ea1ad5fada1f50f1a273d4aac8c14bf6213
SameedIlyas <silyas.bese22seecs@seecs.edu.pk>
2025-08-16 05:35:52 +0500

    Updated Homepage

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -22,6 +22,7 @@ export const MessagesContainer = ({
  }: Props) => {
 
     const bottomRef = useRef<HTMLDivElement>(null);
+    const lastAssistantMessageIDRef = useRef<string | null>(null);
 
     const trpc = useTRPC();
     const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
@@ -31,16 +32,19 @@ export const MessagesContainer = ({
         refetchInterval: 5000,
     }));
 
-/* TODO: This is causing problems
      useEffect(() => {
-        const lastAssistantMessageWithFragment = messages.findLast(
-            (message) => message.role === "ASSISTANT" && !!message.fragment,
+        const lastAssistantMessage = messages.findLast(
+            (message) => message.role === "ASSISTANT"
         );
 
-        if (lastAssistantMessageWithFragment) {
-            setActiveFragment(lastAssistantMessageWithFragment .fragment);
+        if (
+            lastAssistantMessage?.fragment &&
+            lastAssistantMessage.id !== lastAssistantMessageIDRef.current
+        ) {
+            setActiveFragment(lastAssistantMessage.fragment);
+            lastAssistantMessageIDRef.current = lastAssistantMessage.id;
         }
-    }, [messages, setActiveFragment]); */
+    }, [messages, setActiveFragment]);
 
     useEffect(() => {
         bottomRef.current?.scrollIntoView();
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -51,7 +51,7 @@ export const ProjectView = ({ projectId }: Props) => {
                         />
                     </Suspense>      
                 </ResizablePanel>
-                <ResizableHandle withHandle />
+                <ResizableHandle className="hover:bg-primary tranition-colors" />
                 <ResizablePanel
                     defaultSize={65}
                     minSize={50}
